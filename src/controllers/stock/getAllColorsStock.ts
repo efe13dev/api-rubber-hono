765117bbda0929550311d501bf09fc3b2dfa5ec6
efe_13 <efe13dev@gmail.com>
2025-09-13 06:32:04 +0200

    feat: ordena los colores por cantidad en orden descendente en el endpoint de stock

--- src/controllers/stock/getAllColorsStock.ts
+++ src/controllers/stock/getAllColorsStock.ts
@@ -1,10 +1,12 @@
 import type { Context } from "hono";
 
+import { desc } from "drizzle-orm";
+
 import { db } from "../../db";
 import { stockTable } from "../../db/schema";
 
 export async function getAllColorsStock(c: Context) {
-  const stock = await db.select().from(stockTable);
+  const stock = await db.select().from(stockTable).orderBy(desc(stockTable.quantity));
 
   return c.json(stock);
 }
